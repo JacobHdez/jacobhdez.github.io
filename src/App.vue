756dee7885a1c4362b002209f43bc59eb192c6b0
JacobHdez <48144152+JacobHdez@users.noreply.github.com>
2026-02-25 08:29:01 -0600

    feat: basic page structure

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,21 @@
-<template></template>
+<script setup lang="ts">
+import Home from '@/views/Home.vue';
+import Header from '@/components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
+</script>
+
+<template>
+  <div
+    class="flex flex-row bg-neutral-100 text-neutral-950 antialiased dark:bg-neutral-950 dark:text-neutral-100"
+  >
+    <Header />
+
+    <div class="flex min-h-screen flex-1 flex-col px-4 py-2">
+      <main class="grow">
+        <Home />
+      </main>
+
+      <Footer />
+    </div>
+  </div>
+</template>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ThemeToggle from '../ui/ThemeToggle.vue';
+</script>
+
+<template>
+  <header class="relative w-xs">
+    <div class="absolute inset-0">
+      <div class="sticky top-0 bottom-0 left-0 flex h-screen flex-col px-4 py-2">
+        <div class="grow overflow-y-auto"></div>
+
+        <div>
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
